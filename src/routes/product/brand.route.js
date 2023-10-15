0f2c54f49773a7371a35eeb4e5a528907f8445bb
bacnxph19667 <nxb16720@gmail.com>
2023-10-15 07:58:38 +0700

    update

---
+++ src/routes/product/brand.route.js
@@ -0,0 +1,10 @@
+import * as brandController from "../../controllers/product/brand.controller";
+import express from "express";
+const brandRoute = express.Router();
+
+brandRoute.post("/create", brandController.create);
+brandRoute.post("/update/:id", brandController.update);
+brandRoute.delete("/remove/:id", brandController.remove);
+brandRoute.get("/", brandController.read);
+
+export default brandRoute;
