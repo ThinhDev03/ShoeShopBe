62d7a25a0fed4037428d988da3c54a70d54addcd
Bacnxph19667 <nxb167200@gmail.com>
2023-10-26 17:11:06 +0700

    fix: mesage brand

--- src/database/connect.js
+++ src/database/connect.js
@@ -4,8 +4,6 @@ dotenv.config();
 
 //require database URL from properties file
 const DB_URL = `mongodb://127.0.0.1:27017/shoeShop`;
-// const DB_URL =
-//   "mongodb+srv://phamtuan19hd:phamtuan19hd@shoeshopee.jy326ur.mongodb.net/?retryWrites=true&w=majority";
 
 //export this function and imported by server.js
 export const connectMongoDB = () => {
