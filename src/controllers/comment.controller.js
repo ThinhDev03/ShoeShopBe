39e13300895d27174b099609e135376077620c5f
PH27311 <tundph27311@fpt.edu.vn>
2023-11-01 13:46:11 +0700

    comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -33,10 +33,12 @@ export const create = async (req, res) => {
     const body = req.body;
     const data = await commentRepository.create( body );
 
+
     const response = {
       data,
       message: "Tạo bình luận thành công",
     };
+
 
     return responseSuccess(res, response);
   } catch (error) {
@@ -50,12 +52,10 @@ export const update = async (req, res) => {
     const body = req.body;
     const { id } = req.params;
     const data = await commentRepository.update( id, body );
-
-    const response = {
-      data,
-      message: "Cập nhật thành công bình luận ",
-    };
-
+        const response = {
+            data,
+            message: "Cập nhật thành công bình luận ",
+        };
     return responseSuccess(res, response);
   } catch (error) {
     return responseError(res, error);
