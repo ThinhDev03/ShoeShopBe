5daa7f3a25e4d4b77646f44cf6a53b2f382103d3
tuannmph27409 <tuannmph27409@fpt.edu.vn>
2023-11-02 16:16:28 +0700

    update.....

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -35,8 +35,7 @@ export const create = async (req, res) => {
 
     const response = {
       data,
-      message: "Tạo bình luận thành công ! ",
-
+      message: "Tạo bình luận thành công ",
     };
 
 
@@ -55,7 +54,7 @@ export const update = async (req, res) => {
 
     const response = {
       data,
-      message: "Cập nhật bình luận thành công",
+      message: "Cập nhật bình luận thành công ",
     };
 
     return responseSuccess(res, response);
@@ -72,7 +71,7 @@ export const remove = async (req, res) => {
 
     const response = {
       data,
-      message: "Xóa bình luận thành công",
+      message: "Xóa bình luận thành công ",
     };
 
     return responseSuccess(res, response);
