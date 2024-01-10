cc0dcf93c888f9607207465f0f7d3eeb72d58322
tuannmph27409 <tuannmph27409@fpt.edu.vn>
2024-01-10 21:44:41 +0700

    feate: create bill history model and route

---
+++ src/repositories/bill-history.repository.js
@@ -0,0 +1,10 @@
+import billHistioryModel from "../database/models/bill-history";
+import BaseRepository from "../helpers/BaseRepository";
+
+class BillDetailRepository extends BaseRepository {
+    constructor(props) {
+        super(props);
+    }
+}
+
+export default new BillDetailRepository({ model: billHistioryModel });
